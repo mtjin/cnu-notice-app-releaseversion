63bed909befc2968eb5b310da79644ef54616bc7
jackjackE <tmddjs210@naver.com>
2020-06-03 15:46:05 +0900

    [base] update BaseFragment

--- CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/base/BaseFragment.kt
+++ CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/base/BaseFragment.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 
-abstract class BaseFragment<B : ViewDataBinding>(
+abstract class BaseFragment<B : ViewDataBinding, VM : BaseViewModel>(
     @LayoutRes val layoutId: Int
 ) : Fragment() {
-    lateinit var binding: B
+    protected lateinit var binding: B
+    protected abstract val viewModel: VM
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,8 +22,17 @@ abstract class BaseFragment<B : ViewDataBinding>(
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
-        binding.lifecycleOwner = this
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = this
+        init()
+    }
+
+    abstract fun init()
+
+    protected fun showToast(msg: String) =
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
 }
