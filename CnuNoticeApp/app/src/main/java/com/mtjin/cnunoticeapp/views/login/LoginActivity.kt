251356cbdefaba2c54296cae864560785721818b
jackjackE <tmddjs210@naver.com>
2020-06-12 16:25:07 +0900

    [view] add LoginActivity

---
+++ CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/views/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.mtjin.cnunoticeapp.views.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mtjin.cnunoticeapp.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
