144af9ca0b4c0f287ec69fd56499ea276464941e
jackjackE <tmddjs210@naver.com>
2020-06-12 17:35:00 +0900

    [view, splash] change startActivity  Main -> Login

--- CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/views/splash/SplashActivity.kt
+++ CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/views/splash/SplashActivity.kt
@@ -3,12 +3,12 @@ package com.mtjin.cnunoticeapp.views.splash
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.mtjin.cnunoticeapp.views.main.MainActivity
+import com.mtjin.cnunoticeapp.views.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, MainActivity::class.java))
+        startActivity(Intent(this, LoginActivity::class.java))
         finish()
     }
 }
