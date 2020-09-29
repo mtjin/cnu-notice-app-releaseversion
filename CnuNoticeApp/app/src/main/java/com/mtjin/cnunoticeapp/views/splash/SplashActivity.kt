c3868b1dd53c34e31471edacb2436f7f3cc36075
jackjackE <tmddjs210@naver.com>
2020-09-29 23:44:09 +0900

    [Revise] SplashActivity.kt 자동 로그인 구현

--- CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/views/splash/SplashActivity.kt
+++ CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/views/splash/SplashActivity.kt
@@ -3,17 +3,27 @@ package com.mtjin.cnunoticeapp.views.splash
 import android.content.Intent
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.mtjin.cnunoticeapp.views.login.LoginActivity
+import com.mtjin.cnunoticeapp.views.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-        startActivity(Intent(this, LoginActivity::class.java))
+        auth = FirebaseAuth.getInstance()
+        if (auth.currentUser != null) {
+            Toast.makeText(this, "자동 로그인", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
         finish()
     }
 }
