c76e7e6f0f61ff8cf1867f2e815d43539ba1a336
jackjackE <tmddjs210@naver.com>
2020-12-30 00:36:06 +0900

    [Add] BoardActivity.kt 추가

---
+++ CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/views/board/BoardActivity.kt
@@ -0,0 +1,12 @@
+package com.mtjin.cnunoticeapp.views.board
+
+import android.os.Bundle
+import com.mtjin.cnunoticeapp.R
+import com.mtjin.cnunoticeapp.base.BaseActivity
+import com.mtjin.cnunoticeapp.databinding.ActivityBoardBinding
+
+class BoardActivity : BaseActivity<ActivityBoardBinding>(R.layout.activity_board) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
