1f16ac50be849425010cfb0a114fdb6225ae3111
jackjackE <tmddjs210@naver.com>
2020-12-29 20:02:16 +0900

    [Add] MainActivity.kt 추가

---
+++ CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/views/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mtjin.cnunoticeapp.views.main
+
+import android.os.Bundle
+import com.mtjin.cnunoticeapp.R
+import com.mtjin.cnunoticeapp.base.BaseActivity
+import com.mtjin.cnunoticeapp.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
