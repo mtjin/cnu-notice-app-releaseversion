df0cc8e638b6dcd0c232973e70e0a3609481eb9b
jackjackE <tmddjs210@naver.com>
2020-12-31 19:26:22 +0900

    [Add] SharedPrefManager.kt 추가

---
+++ CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/utils/SharedPrefManager.kt
@@ -0,0 +1,22 @@
+package com.mtjin.cnunoticeapp.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class SharedPrefManager(context: Context) {
+    private val sharedPref: SharedPreferences =
+        context.getSharedPreferences(CNU_APP, Context.MODE_PRIVATE)
+
+    var univAuth: Boolean
+        get() = sharedPref.getBoolean(UNIV_AUTH_KEY, false)
+        set(value) {
+            val editor = sharedPref.edit()
+            editor.putBoolean(UNIV_AUTH_KEY, value)
+            editor.apply()
+        }
+
+    companion object {
+        private const val CNU_APP = "CNU_APP"
+        private const val UNIV_AUTH_KEY = "UNIV_AUTH_KEY" //대학교 인증
+    }
+}
