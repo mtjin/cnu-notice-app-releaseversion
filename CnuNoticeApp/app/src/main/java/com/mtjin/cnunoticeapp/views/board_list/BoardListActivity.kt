e3124eff491f9622153491367103dedcdc2c0b10
jackjackE <tmddjs210@naver.com>
2021-01-03 18:23:42 +0900

    [Implement] BoardListActivity.kt 게시판 타이틀 텍스트 세팅

--- CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/views/board_list/BoardListActivity.kt
+++ CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/views/board_list/BoardListActivity.kt
@@ -4,9 +4,16 @@ import android.os.Bundle
 import com.mtjin.cnunoticeapp.R
 import com.mtjin.cnunoticeapp.base.BaseActivity
 import com.mtjin.cnunoticeapp.databinding.ActivityBoardListBinding
+import com.mtjin.cnunoticeapp.utils.constants.EXTRA_BOARD_NAME
 
 class BoardListActivity : BaseActivity<ActivityBoardListBinding>(R.layout.activity_board_list) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        processIntent()
+    }
+
+    private fun processIntent() {
+        val boardName = intent.getStringExtra(EXTRA_BOARD_NAME)
+        binding.tvTitle.text = boardName
     }
 }
