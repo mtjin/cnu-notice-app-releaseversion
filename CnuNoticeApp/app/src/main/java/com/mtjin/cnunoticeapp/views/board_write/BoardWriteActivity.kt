e5d9dc72f5fb1a84d9c2215e802722c2fcfc8d5e
jackjackE <tmddjs210@naver.com>
2021-01-03 18:20:04 +0900

    [Add] BoardWriteActivity.kt 추가

---
+++ CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/views/board_write/BoardWriteActivity.kt
@@ -0,0 +1,12 @@
+package com.mtjin.cnunoticeapp.views.board_write
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mtjin.cnunoticeapp.R
+
+class BoardWriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board_write)
+    }
+}
