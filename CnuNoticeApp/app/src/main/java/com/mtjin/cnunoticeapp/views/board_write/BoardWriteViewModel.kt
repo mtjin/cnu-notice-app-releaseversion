5b52d97124527ded797457af317407f53e4fcde3
jackjackE <tmddjs210@naver.com>
2021-01-06 21:39:02 +0900

    [Implement] BoardWriteViewModel.kt 구조 잡음

--- CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/views/board_write/BoardWriteViewModel.kt
+++ CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/views/board_write/BoardWriteViewModel.kt
@@ -1,7 +1,32 @@
 package com.mtjin.cnunoticeapp.views.board_write
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.mtjin.cnunoticeapp.base.BaseViewModel
+import com.mtjin.cnunoticeapp.utils.SingleLiveEvent
 
 class BoardWriteViewModel : BaseViewModel() {
     lateinit var boardName: String
+
+    private val _emptyMsg = SingleLiveEvent<String>()
+    private val _insertBoardSuccess = SingleLiveEvent<Boolean>()
+
+    val emptyMsg: LiveData<String> get() = _emptyMsg
+    val insertBoardSuccess: LiveData<Boolean> get() = _insertBoardSuccess
+
+    var title = MutableLiveData("")
+    var content = MutableLiveData("")
+
+
+    //작성완료
+    fun insertBoard() {
+        when {
+            content.value.isNullOrBlank() || title.value.isNullOrBlank() -> _emptyMsg.call()
+            else -> { //작성조건 충족
+
+            }
+        }
+    }
+
+
 }
