3e3f6d030b847bf5dde91ffe8a18be66436d93c7
jackjackE <tmddjs210@naver.com>
2021-01-09 14:58:01 +0900

    [Add] YesNoDialogFragment.kt 네, 아니요 선택 다이얼로그 프래그먼트 추가

---
+++ CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/views/dialog/YesNoDialogFragment.kt
@@ -0,0 +1,41 @@
+package com.mtjin.cnunoticeapp.views.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.mtjin.cnunoticeapp.R
+import kotlinx.android.synthetic.main.fragment_dialog_yes_no.view.*
+
+class YesNoDialogFragment : DialogFragment(),
+    View.OnClickListener {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_dialog_yes_no, container, false)
+        view.tv_yes.setOnClickListener {
+            yesClick(true)
+            dismiss()
+        }
+        view.tv_no.setOnClickListener {
+            dismiss()
+        }
+
+        return view
+    }
+
+    companion object {
+        lateinit var yesClick: (Boolean) -> Unit
+        fun getInstance(yesClick: (Boolean) -> Unit): YesNoDialogFragment {
+            this.yesClick = yesClick
+            return YesNoDialogFragment()
+        }
+    }
+
+    override fun onClick(p0: View?) {
+        dismiss()
+    }
+}
