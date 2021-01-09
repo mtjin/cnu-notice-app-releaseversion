7a56e47ddf62d3eff35de6c66275c5158bfa9192
jackjackE <tmddjs210@naver.com>
2021-01-09 17:18:39 +0900

    [Add] AppNoticeActivity.kt 추가

---
+++ CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/views/app_notice/AppNoticeActivity.kt
@@ -0,0 +1,12 @@
+package com.mtjin.cnunoticeapp.views.app_notice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mtjin.cnunoticeapp.R
+
+class AppNoticeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_notice)
+    }
+}
