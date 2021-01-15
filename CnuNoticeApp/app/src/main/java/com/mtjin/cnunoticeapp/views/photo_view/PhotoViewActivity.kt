aa2edeb288cf5765096a9f2ae8d0a990fcf94735
jackjackE <tmddjs210@naver.com>
2021-01-15 22:56:30 +0900

    [Add] PhotoViewActivity.kt 추가

---
+++ CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/views/photo_view/PhotoViewActivity.kt
@@ -0,0 +1,13 @@
+package com.mtjin.cnunoticeapp.views.photo_view
+
+import android.os.Bundle
+import com.mtjin.cnunoticeapp.R
+import com.mtjin.cnunoticeapp.base.BaseActivity
+import com.mtjin.cnunoticeapp.databinding.ActivityPhotoViewBinding
+
+class PhotoViewActivity : BaseActivity<ActivityPhotoViewBinding>(R.layout.activity_photo_view) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
