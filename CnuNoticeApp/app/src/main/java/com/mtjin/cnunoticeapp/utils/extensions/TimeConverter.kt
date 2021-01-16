21fcc9e8cbec6f22a817781f1bfd71ac6bafcdc7
jackjackE <tmddjs210@naver.com>
2021-01-16 18:44:25 +0900

    [Fix] 하루차이가 아닌 24시간 차이로 시간 설정되던 버그 픽스

--- CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/utils/extensions/TimeConverter.kt
+++ CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/utils/extensions/TimeConverter.kt
@@ -1,22 +1,26 @@
 package com.mtjin.cnunoticeapp.utils.extensions
 
 import android.text.format.DateFormat
+import android.util.Log
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 //현재 타임스템프 얻기
-fun getTimestamp() = Calendar.getInstance().timeInMillis
+fun getTimestamp() = Calendar.getInstance(Locale.KOREA).timeInMillis
 
 fun Long.convertBoardTime(): String {
-    val now = Calendar.getInstance()
-    val diffInMillis = now.timeInMillis - this
-    val diffHour = diffInMillis / 1000 / 60 / 60L
-    return if (diffHour < 1) { //분 단위 전
+    val now = Calendar.getInstance(Locale.KOREA)
+    val diffInMillis = now.timeInMillis - this //현재와의 밀리초 차이
+    val todayOfDay = TimeUnit.MILLISECONDS.toDays(now.timeInMillis)
+    val thisOfDay = TimeUnit.MILLISECONDS.toDays(this)
+    val currentDiffHour = diffInMillis / 1000 / 60 / 60L //현재와 시간 차이
+    return if (currentDiffHour < 1) { //현재시간 60분 전
         (diffInMillis / 1000 / 60L).toString() + "분 전"
     } else {
-        if (diffHour < 24) { //시간 단위 전 (08:23)
-            this.convertHourMinute()
-        } else { // 일 수 전 (2021.01.07 08:23)
-            this.convertDateTimeMinute()
+        if (todayOfDay == thisOfDay) { //오늘
+            this.convertHourMinute() //(08:23)
+        } else { // 하루 이상 지난 날
+            this.convertDateTimeMinute() // (2021.01.07 08:23)
         }
     }
 }
