c6edae55ce5051bae9cf16674a82c30c03855be6
jackjackE <tmddjs210@naver.com>
2021-01-24 16:30:51 +0900

    [Implement] SplashActivity.kt RemoteConfig로 앱 버전차이 다를 경우 업데이트 요청

--- CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/views/splash/SplashActivity.kt
+++ CnuNoticeApp/app/src/main/java/com/mtjin/cnunoticeapp/views/splash/SplashActivity.kt
@@ -1,17 +1,26 @@
 package com.mtjin.cnunoticeapp.views.splash
 
+
 import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.mtjin.cnunoticeapp.utils.SharedPrefManager
+import com.mtjin.cnunoticeapp.utils.constants.APP_VERSION_CODE
 import com.mtjin.cnunoticeapp.views.login.LoginActivity
 import com.mtjin.cnunoticeapp.views.main.MainActivity
 
+
 class SplashActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    val mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setFlags(
@@ -19,13 +28,42 @@ class SplashActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
         auth = FirebaseAuth.getInstance()
-        if (auth.currentUser != null) {
-            Toast.makeText(this, "자동 로그인", Toast.LENGTH_SHORT).show()
-            SharedPrefManager(this).uuid = auth.currentUser!!.uid
-            startActivity(Intent(this, MainActivity::class.java))
-        } else {
-            startActivity(Intent(this, LoginActivity::class.java))
+        val appVersion = mFirebaseRemoteConfig.getString(APP_VERSION_CODE)
+        if (appVersion == getAppVersionCode()) {
+            if (auth.currentUser != null) {
+                Toast.makeText(this, "자동 로그인", Toast.LENGTH_SHORT).show()
+                SharedPrefManager(this).uuid = auth.currentUser!!.uid
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+            finish()
+        } else { //앱 버전이 낮을경우 업데이트 필요
+            AlertDialog.Builder(this).apply {
+                setTitle("안내")
+                setMessage("앱 업데이트가 필요합니다.")
+                setPositiveButton(
+                    "업데이트"
+                ) { _, _ ->
+                    val appUpdateIntent = Intent(Intent.ACTION_VIEW)
+                    appUpdateIntent.data = Uri.parse("market://details?id=$packageName")
+                    startActivity(appUpdateIntent)
+                    finish()
+                }
+                this.show()
+            }
         }
-        finish()
+    }
+
+
+    private fun getAppVersionCode(): String {
+        try {
+            val pInfo: PackageInfo =
+                this.packageManager.getPackageInfo(this.packageName, 0)
+            return pInfo.versionName
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+        return ""
     }
 }
